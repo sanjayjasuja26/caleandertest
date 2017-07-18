8756a8a01a101bed241a64759127a1e3f90cb6c1
Rajender <rthakur.dev@gmail.com>
2017-07-18 14:09:01 +0530

    reset caledaner

--- app/Http/Controllers/CaleanderController.php
+++ app/Http/Controllers/CaleanderController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Event;
+use Auth;
 class CaleanderController extends Controller
 {
     public function getindex()
     {
-     return view('user.caleander.index');
+      $data['dates'] = Event::where('user_id',Auth::id())->pluck('date');
+
+     return view('user.caleander.index',$data);
     }
 }
